2eb0ab24a99eff9f238ec505153d80bbf36192d2
25LP1024 25LP1024 <25LP1024@G301-CC0195.local>
2025-09-23 14:17:19 +0800

    9.23

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/home/theme-provider";
+import { Header } from "@/components/main/Header";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -33,6 +34,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Header />
           {children}
         </ThemeProvider>
       </body>
--- components/main/header.tsx
+++ components/main/header.tsx
@@ -1,3 +1,28 @@
+import { Search } from "lucide-react";
+import { ThemeToggler } from "../home";
+import { Input } from "../ui/input";
+import { GenreDropdown } from "./GenreDropdown";
+import { SearchSection } from "./SearchSection";
+
 export const Header = () => {
-  return <div>header</div>;
+  return (
+    <header className="w-full">
+      <div className="max-w-[1280px] flex justify-between m-auto items-center py-3">
+        <div>
+          <img src="/images/film.png" className="h-5" />
+          <p>Movie Z</p>
+        </div>
+        <div className="flex gap-5">
+          <GenreDropdown />
+          <div className="flex items-center">
+            <Search className="-mr-8" />
+            <SearchSection />
+          </div>
+        </div>
+        <div>
+          <ThemeToggler />
+        </div>
+      </div>
+    </header>
+  );
 };
